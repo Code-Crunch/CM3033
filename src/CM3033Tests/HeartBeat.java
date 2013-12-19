d6e6253d3472ac55f7759adc8f434709be271704
FlorinMazilu <mazy_flo@yahoo.com>
2013-12-19 14:47:06 +0200

    asd

--- src/CM3033Tests/HeartBeat.java
+++ src/CM3033Tests/HeartBeat.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cm3033tests;
+package cm3033Tests;
 
 import java.util.Random;
 import javax.swing.JOptionPane;
